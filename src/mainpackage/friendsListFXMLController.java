bca563f174a14eb6a5f876354cb89728ba4a75d5
chsmorg <58636766+chsmorg@users.noreply.github.com>
2021-11-06 12:41:07 -0500

    Menu bar added to friendslist

--- src/mainpackage/friendsListFXMLController.java
+++ src/mainpackage/friendsListFXMLController.java
@@ -1,17 +1,18 @@
 package mainpackage;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 /**
  * Author: Jacob
@@ -40,7 +41,28 @@ public class friendsListFXMLController implements Initializable, PropertyChangeL
     private Text inviteLinkTextarr;
 
     @FXML
-    private Button backButton;
+    private Label coinBalLabel;
+
+    @FXML
+    private Label userNameLabel;
+
+    @FXML
+    void backPressed(ActionEvent event) throws IOException {
+        ScreenController.activate("mainPage");
+
+    }
+
+    @FXML
+    void profilePressed(ActionEvent event) throws IOException {
+       ScreenController.activate("userProfileFXML");
+
+    }
+
+
+    @FXML
+    void addCoinsPressed(ActionEvent event) {
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
