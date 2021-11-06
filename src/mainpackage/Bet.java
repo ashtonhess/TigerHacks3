2a39f793239ccce108cdfb721849b74faf5c64b7
chsmorg <58636766+chsmorg@users.noreply.github.com>
2021-11-05 21:04:22 -0500

    Bet Obj

---
+++ src/mainpackage/Bet.java
@@ -0,0 +1,19 @@
+package mainpackage;
+
+import java.util.Date;
+
+public class Bet {
+    public boolean active;
+    public int betAmount;
+    public String betTarget;
+    public String username;
+    public String title;
+    public boolean betWon;
+    private int betID;
+    public String description;
+    public Date betDate;
+
+
+
+
+}
