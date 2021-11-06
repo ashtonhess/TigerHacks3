055fd45c00899b0dfbe158621e778559676992cf
ashtonhess <ashton.hess@yahoo.com>
2021-11-06 04:49:50 -0500

    Fixing screens to work. Also adding screens to the ScreenController so it can switch between them.

--- src/mainpackage/betmeMain.java
+++ src/mainpackage/betmeMain.java
@@ -16,14 +16,13 @@ public class betmeMain extends Application {
             Scene scene = new Scene(root);
 
             ScreenController.main = scene;
-
-            ScreenController.addScreen("homeFXML", FXMLLoader.load(getClass().getResource("loginFXML.fxml")));
-
     // USE THESE METHODS TO ADD MORE SCREENS TO SWITCH TO
-            //ScreenController.addScreen("", FXMLLoader.load(getClass().getResource("")));
-            //ScreenController.addScreen("", FXMLLoader.load(getClass().getResource("")));
+            ScreenController.addScreen("loginFXML", FXMLLoader.load(getClass().getResource("loginFXML.fxml")));
+            ScreenController.addScreen("newBetFXML", FXMLLoader.load(getClass().getResource("newBetFXML.fxml")));
+            ScreenController.addScreen("userProfileFXML", FXMLLoader.load(getClass().getResource("userProfileFXML.fxml")));
 
-            ScreenController.activate("homeFXML");
+    //Activating the login screen.
+            ScreenController.activate("loginFXML");
 
             stage.setScene(scene);
             stage.show();
