41d7b62b87d63d44052844deaf39fdbab8eb6123
JabokTheGod <jlramirez1117@gmail.com>
2021-11-06 08:59:02 -0500

    Added UI for User Profile with userProfileFXML.fxml, userProfileFXMLController.java

--- src/mainpackage/userProfileFXMLController.java
+++ src/mainpackage/userProfileFXMLController.java
@@ -1,10 +1,80 @@
 package mainpackage;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
-public class userProfileFXMLController {
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.net.URL;
+import java.util.ResourceBundle;
+/**
+ * Author: Jacob
+ */
+public class userProfileFXMLController implements Initializable, PropertyChangeListener {
 
-    
+    @FXML
+    private Text accountBalText;
 
+    @FXML
+    private Button friendRequestsButton;
+
+    @FXML
+    private ListView<?> friendsListListView;
+
+    @FXML
+    private ImageView userPicImageView;
+
+    @FXML
+    private Text friendsListText;
+
+    @FXML
+    private Text totalMoneyText;
+
+    @FXML
+    private Text closedBetsText;
+
+    @FXML
+    private Circle userPicBorderCircle;
+
+    @FXML
+    private Text usernameText;
+
+    @FXML
+    private Button betRequestsButton;
+
+    @FXML
+    private Text activeBetsText;
+
+    @FXML
+    private ListView<?> activeBetsListView;
+
+    @FXML
+    private ListView<?> closedBetsListView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+//        daModel = new loginFXMLModel();
+//        daModel.addPropertyChangeListener(this);
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+        if (evt.getPropertyName().equals("textToChangePropertyChangeID")) {
+            //System.out.println("Some shit");
+//            textToChange.setText((String)evt.getNewValue());
+        }
+    }
+//    @FXML
+//    private void handleDaButton(ActionEvent event) {
+//        daModel.changeDaText();
+//    }
 }
