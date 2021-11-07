94e8a565f931cd2d31642a9ff081512e3e741961
ashtonhess <ashton.hess@yahoo.com>
2021-11-07 01:14:26 -0600

    Adding Portfolio and User classes. Fields and full constructors are done.

---
+++ src/mainpackage/Portfolio.java
@@ -0,0 +1,18 @@
+package mainpackage;
+
+public class Portfolio {
+    public String userID;
+    public Integer portfolioBalance;
+    public Integer portfolioTotalReturns;
+
+
+    public Portfolio(String userID, Integer portfolioBalance, Integer portfolioTotalReturns){
+        this.userID = userID;
+        this.portfolioBalance = portfolioBalance;
+        this.portfolioTotalReturns = portfolioTotalReturns;
+    }
+
+
+}
+
+
---
+++ src/mainpackage/User.java
@@ -0,0 +1,16 @@
+package mainpackage;
+
+public class User {
+
+    public String userID;
+    public String userPassword;
+    public String userDateCreated;
+
+    public User(String userID, String userPassword, String userDateCreated){
+        this.userID = userID;
+        this.userPassword = userPassword;
+        this.userDateCreated = userDateCreated;
+    }
+
+
+}
