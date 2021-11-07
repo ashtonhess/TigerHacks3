67e07abf0b478b4d689e5c0160842abba31475ea
ashtonhess <ashton.hess@yahoo.com>
2021-11-06 21:19:57 -0500

    Working on createaccount controller. implementing db to create an account.

--- src/mainpackage/createAccountFXMLController.java
+++ src/mainpackage/createAccountFXMLController.java
@@ -9,34 +9,26 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import javafx.util.Pair;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.ResultSet;
 import java.util.ResourceBundle;
 /**
  * Author: Jacob
  */
 public class createAccountFXMLController implements Initializable, PropertyChangeListener {
 
+    public daDatabase databaseObj = new daDatabase();
+
     @FXML
     private TextField passwordTextField;
 
     @FXML
-    private Text passwordText;
-
-    @FXML
-    private TextField emailTextField;
-
-    @FXML
-    private Text usernameText;
-
-    @FXML
-    private Text emailText;
-
-    @FXML
-    private Button doneButton;
+    private Button createButton;
 
     @FXML
     private TextField usernameTextField;
@@ -58,16 +50,65 @@ public class createAccountFXMLController implements Initializable, PropertyChang
     }
 
     @FXML
-    void donePressed(ActionEvent event) throws IOException {
+    void handleCreateButton(ActionEvent event) throws IOException {
 
-        //code
-        ScreenController.activate("loginFXML");
+        String usernameInput = usernameTextField.getText();
+        String passwordInput = passwordTextField.getText();
+        Pair<Boolean, Pair<Integer,ResultSet>> databaseResult = new Pair<Boolean, Pair<Integer,ResultSet>>(false, new Pair<Integer, ResultSet>(0,null));
+
+
+
+
+       // return new Pair<Boolean, Pair<Integer, ResultSet>>(false, new Pair<Integer, ResultSet>(0, null));
+
+        databaseResult = databaseObj.executeQuery("");
+
+        //This is the code to get information out of the databaseResult that is returned by DB.
+        Boolean databaseBoolResult;
+        Integer databaseRowsResult;
+        ResultSet databaseResultSet;
+        databaseBoolResult = databaseResult.getKey();
+        databaseRowsResult = databaseResult.getValue().getKey();
+        databaseResultSet = databaseResult.getValue().getValue();
+
+
+        /*
+        * //executeQuery function.
+    //INPUTS: String: a query written exactly as it should be.
+    //OUTPUTS: Boolean: if the query was successfully executed or not.
+    //         Integer: the number of rows in the resulting set.
+    //         ResultSet: the set that the query returned.
+    public Pair<Boolean,Pair<Integer,ResultSet>> executeQuery(String queryString){
+        Connection executeQueryConn = null;
+//        Pair<Boolean,ResultSet> resultPair = new Pair(false,null);
+        ResultSet executeQueryResult;
+        Integer numRows;
+        try{
+            executeQueryConn = getConnection();
+            if (this.connectionStatus(executeQueryConn)==true) {
+                Statement stmt = executeQueryConn.createStatement();
+                numRows= stmt.executeUpdate(queryString);
+                executeQueryResult =  stmt.executeQuery(queryString);
+                return new Pair<Boolean, Pair<Integer, ResultSet>>(true, new Pair<Integer, ResultSet>(numRows, executeQueryResult));
+            }else{
+                return new Pair<Boolean, Pair<Integer, ResultSet>>(false, new Pair<Integer, ResultSet>(0, null));
+            }
+        }catch(SQLException sqlExcept){
+            System.out.println("getConnection() failed. Database error.");
+            return new Pair<Boolean, Pair<Integer, ResultSet>>(false, new Pair<Integer, ResultSet>(0, null));
+        }
+    }
+        *
+        * */
+
+        //ScreenController.activate("loginFXML");
+
 
 
     }
 
     @FXML
-    void cancelPressed(ActionEvent event) {
+    void handleCancelPressed(ActionEvent event) {
         ScreenController.activate("loginFXML");
 
     }
