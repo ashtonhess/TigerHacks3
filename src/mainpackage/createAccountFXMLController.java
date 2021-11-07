3b3aedb46905c5830c02124ba95595b4a132fe9f
ashtonhess <ashton.hess@yahoo.com>
2021-11-06 23:06:49 -0500

    Did work on creating an account. Just pasted code that was previously done but not pushed. Most work done in handleCreateButton in createAccountFXMLController.java.

--- src/mainpackage/createAccountFXMLController.java
+++ src/mainpackage/createAccountFXMLController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -16,11 +17,14 @@ import java.beans.PropertyChangeListener;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 /**
  * Author: Jacob
  */
 public class createAccountFXMLController implements Initializable, PropertyChangeListener {
+
+    Boolean holderBool = false;
 
     public daDatabase databaseObj = new daDatabase();
 
@@ -52,57 +56,88 @@ public class createAccountFXMLController implements Initializable, PropertyChang
     @FXML
     void handleCreateButton(ActionEvent event) throws IOException {
 
-        String usernameInput = usernameTextField.getText();
-        String passwordInput = passwordTextField.getText();
+
+
+
         Pair<Boolean, Pair<Integer,ResultSet>> databaseResult = new Pair<Boolean, Pair<Integer,ResultSet>>(false, new Pair<Integer, ResultSet>(0,null));
 
-       // return new Pair<Boolean, Pair<Integer, ResultSet>>(false, new Pair<Integer, ResultSet>(0, null));
+        Boolean databaseBoolResult;
+        Integer databaseRowsResult;
+        ResultSet databaseResultSet;
+
+        if(holderBool == false){
+            String usernameInput = usernameTextField.getText();
+            String passwordInput = passwordTextField.getText();
+            if (usernameInput == "" | passwordInput == ""){
+                Alert emptyInputAlert = new Alert(Alert.AlertType.ERROR);
+                emptyInputAlert.setContentText("Please enter a valid username and password.");
+                emptyInputAlert.showAndWait();
+                holderBool = false;
+            }else{
+                databaseResult = databaseObj.executeQuery(constructCheckUserIDExistsQuery(usernameInput));
+                databaseBoolResult = databaseResult.getKey();
+                databaseRowsResult = databaseResult.getValue().getKey();
+                databaseResultSet = databaseResult.getValue().getValue();
+                if (databaseBoolResult == false){
+                    Alert boolResultFalseAlert = new Alert(Alert.AlertType.ERROR);
+                    boolResultFalseAlert.setContentText("There was an error executing query. Check DB connection.");
+                    boolResultFalseAlert.showAndWait();
+                }else{
+                    try {
+                        if (databaseResultSet.next() == false){
+                            //this means that another user with that username DOES NOT exist.
+                            //continue to add new user to database.
+
+
+                            //left off
+                            //committing these now...
+
+                            databaseResult = databaseObj.executeQuery(constructNewUserQuery(usernameInput, passwordInput));
+                            System.out.println("New user: "+usernameInput+" has been added to the UserTable.");
+                            Alert emptyInputAlert = new Alert(Alert.AlertType.CONFIRMATION);
+                            emptyInputAlert.setContentText("New user: "+usernameInput+" has been added to the UserTable.");
+                            emptyInputAlert.showAndWait();
+                            ScreenController.activate("loginFXML");
+                        }
+                    } catch (SQLException e) {
+                        System.out.println("DATABASE ERROR. createAccountFXMLController.java @ handleCreateButton");
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
+
+
+
+        // return new Pair<Boolean, Pair<Integer, ResultSet>>(false, new Pair<Integer, ResultSet>(0, null));
 
         databaseResult = databaseObj.executeQuery("");
 
         //This is the code to get information out of the databaseResult that is returned by DB.
-        Boolean databaseBoolResult;
-        Integer databaseRowsResult;
-        ResultSet databaseResultSet;
-        databaseBoolResult = databaseResult.getKey();
-        databaseRowsResult = databaseResult.getValue().getKey();
-        databaseResultSet = databaseResult.getValue().getValue();
 
-        /*
-        * //executeQuery function.
-    //INPUTS: String: a query written exactly as it should be.
-    //OUTPUTS: Boolean: if the query was successfully executed or not.
-    //         Integer: the number of rows in the resulting set.
-    //         ResultSet: the set that the query returned.
-    public Pair<Boolean,Pair<Integer,ResultSet>> executeQuery(String queryString){
-        Connection executeQueryConn = null;
-//        Pair<Boolean,ResultSet> resultPair = new Pair(false,null);
-        ResultSet executeQueryResult;
-        Integer numRows;
-        try{
-            executeQueryConn = getConnection();
-            if (this.connectionStatus(executeQueryConn)==true) {
-                Statement stmt = executeQueryConn.createStatement();
-                numRows= stmt.executeUpdate(queryString);
-                executeQueryResult =  stmt.executeQuery(queryString);
-                return new Pair<Boolean, Pair<Integer, ResultSet>>(true, new Pair<Integer, ResultSet>(numRows, executeQueryResult));
-            }else{
-                return new Pair<Boolean, Pair<Integer, ResultSet>>(false, new Pair<Integer, ResultSet>(0, null));
-            }
-        }catch(SQLException sqlExcept){
-            System.out.println("getConnection() failed. Database error.");
-            return new Pair<Boolean, Pair<Integer, ResultSet>>(false, new Pair<Integer, ResultSet>(0, null));
-        }
-    }
-        * */
+
+
 
         //ScreenController.activate("loginFXML");
 
     }
 
+    public String constructCheckUserIDExistsQuery(String usernameInput){
+        String constructCheckUserIdExistsQueryString = "SELECT * FROM UserTable WHERE userID = '"+usernameInput+"';";
+        return constructCheckUserIdExistsQueryString;
+    }
+    public String constructNewUserQuery(String usernameInput, String passwordInput){
+        String constructNewUserQueryString = "INSERT INTO User (userID, userPassword, userDateCreated)\n" +
+                "VALUES ('"+usernameInput+"', '"+passwordInput+"', '03/16/2001 08:42:09');";
+
+        return constructNewUserQueryString;
+    }
+
     @FXML
     void handleCancelPressed(ActionEvent event) {
         ScreenController.activate("loginFXML");
-
     }
+
 }
