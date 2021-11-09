e3985c8709499366ca759c55c89ccbbe463ef2a2
chsmorg <58636766+chsmorg@users.noreply.github.com>
2021-11-08 20:25:54 -0600

    start of internal functions bug fix

---
+++ src/mainpackage/AbstractDataController.java
@@ -0,0 +1,12 @@
+package mainpackage;
+
+import java.util.ArrayList;
+
+public abstract class AbstractDataController {
+    public static int betIDs = 0;
+    public static User userProfile = null;
+
+
+
+
+}
